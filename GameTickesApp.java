c2779f0c00aa9c5e8358606c97211a047609f9fe
BasopuA <anelebasopu@gmail.com>
2024-08-09 18:31:20 +0200

    Add files via upload

---
+++ GameTickesApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gametickesapp;
+
+import za.ac.tut.gui.GameTickets;
+
+/**
+ *
+ * @author Alex
+ */
+public class GameTickesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        new GameTickets();
+    }
+    
+}
